1f740c08a6fee62743ff7e60fcd67cbd9c087015
maloong <maloong2022@gmail.com>
2023-09-08 20:08:16 +0800

    Update openai.ts

--- convex/openai.ts
+++ convex/openai.ts
@@ -1,13 +1,15 @@
 "use node";
-import { Configuration, OpenAIApi } from "openai";
+
 import { action } from "./_generated/server";
 import { v } from "convex/values";
 import { api } from "../convex/_generated/api";
+import OpenAI from 'openai';
+
 
 // Initialize the OpenAI client with the given API key
-const apiKey = process.env.OPENAI_API_KEY!;
-const openai = new OpenAIApi(new Configuration({ apiKey }));
-
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+});
 // start a game
 export const startGames = action({
     args: {
@@ -16,13 +18,14 @@ export const startGames = action({
       roomId: v.id("rooms")
     },
     handler: async (ctx, args) => {
-        const response = await openai.createCompletion({
+
+        const response = await openai.completions.create({
             model: "text-davinci-003",
             prompt:`Suggest ${args.numberOfPlayer} words  for an topic that is '${args.topic}'`,
           });
       
           // Pull the message content out of the response
-          const words = response['choices'][0]['text'];
+          const words = response.choices[0].text;
           // save the words and the topic into the database
           await ctx.runMutation(api.player.startGames, { topic:args.topic,words: words,roomId:args.roomId });
           return words;
