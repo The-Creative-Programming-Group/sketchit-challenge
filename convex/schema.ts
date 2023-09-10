3c7341406e9453e6d01a258680b325d131ab5efd
tretyakovalex <60971868+tretyakovalex@users.noreply.github.com>
2023-09-10 23:11:41 +0400

    removed played defineTable schema.ts

--- convex/schema.ts
+++ convex/schema.ts
@@ -7,12 +7,6 @@ export default defineSchema({
         topic: v.string(),
         words: v.string(),
     }),
-    player: defineTable({
-        username:v.string(),
-        roomId:v.id("rooms"),
-        gameId:v.optional(v.id("games")),
-        score: v.optional(v.number()),
-    }),
     rooms: defineTable({
         roomName:v.string(),
     })
